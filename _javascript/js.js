8b56f70d0369facdb8ebc499766ebdffdc16437c
Zetoo <48632104>
2015-01-08 21:46:01 +0100

    le site v2
    css html

--- _javascript/js.js
+++ _javascript/js.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 //le transition
 window.onload = function() {
-	$('body').delay(5000).queue(function(){
+	$('body').delay(00).queue(function(){
 		  $('.loader-overlay').removeClass('hidden').clearQueue();
 		  $('.loader').addClass('quit-loader').clearQueue();
 		  $('.accueil').addClass('start-accueil').clearQueue();
@@ -21,10 +21,51 @@ $(document).ready(function() {
 		});
 	});
 	
-//le slide up
-jQuery(document).ready(function() {
-    jQuery('.post').addClass("hidden").viewportChecker({
-        classToAdd: 'visible animated fadeIn',
-        offset: 100
-       });
-});
+//le slider
+
+jQuery(document).ready(function ($) {
+
+  $('#checkbox').change(function(){
+    setInterval(function () {
+        moveRight();
+    }, 3000);
+  });
+  
+	var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+	
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+	
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
+});    
